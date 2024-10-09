6e2409ab68a957b7ba42f48963959e040ea4d2fa
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-09 14:52:27 +0200

    cambio un poco el front pero sin hacer ningun cambio del back

--- ejercicios_navigation/App.js
+++ ejercicios_navigation/App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text, StyleSheet, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function Principal({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>hola mundete</Text>
-      <StatusBar style="auto" />
+    <View style={styles.HomeScreen}>
+      <Text style={styles.Encabezado}>Productos</Text>
+      <FlatList style={styles.ListaProductos}>
+        
+      </FlatList>
     </View>
   );
 }
 
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Principal} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  HomeScreen: {
+    backgroundColor: "#b3c0c5",
+    alignItems: "center",
   },
-});
+
+  Encabezado: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#30225a'
+  },
+
+  ListaProductos: {
+    width: '100%',
+    paddingHorizontal: 10,
+    marginBottom: 20
+  }
+
+})
+
+export default App;
