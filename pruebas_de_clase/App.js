5343efb10efb3ea142e2b191f725c25723b4b876
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-18 10:35:48 +0200

    hacemos pruebas con fetch, pero no funciona

--- pruebas_de_clase/App.js
+++ pruebas_de_clase/App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Pressable} from "react-native";
 
+const URL = 'https://dummyjson.com/recipes/1';
 export default function App() {
-  return (
+  const [receta, setReceta] = useState(null);
+
+  const obtenerReceta=()=>{
+    fetch(URL)
+   .then(res => res.json())
+   .then(console.log('Receta obtenida:', receta))
+   .catch(error=>console.error(error));
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable onPress={obtenerReceta}>
+        <Text>Obtener receta</Text>
+      </Pressable>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+ 
