12ddbd9eff55f4f52011cdb900b2582846da6ba4
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-10-22 14:42:38 +0200

    borro cosas insignificantes

--- pruebas_de_clase/App.js
+++ pruebas_de_clase/App.js
@@ -1,33 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Pressable} from "react-native";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import UserInfo from './Components/UserInfo';
 
-const URL = 'https://dummyjson.com/recipes/1';
-export default function App() {
-  const [receta, setReceta] = useState(null);
-
-  const obtenerReceta=()=>{
-    fetch(URL)
-   .then(res => res.json())
-   .then(console.log('Receta obtenida:', receta))
-   .catch(error=>console.error(error));
-  }
-
-  return(
+const App = () => {
+  return (
     <View style={styles.container}>
-      <Pressable onPress={obtenerReceta}>
-        <Text>Obtener receta</Text>
-      </Pressable>
+      <UserInfo />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
   },
 });
- 
+
+export default App;
