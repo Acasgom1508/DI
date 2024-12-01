5e837b2b4bdeb4c42e460526d0f44da0588877ef
Acasgom1508 <166312439+Acasgom1508@users.noreply.github.com>
2024-12-01 20:10:48 +0100

    terminada la pantalla principal
    mañana vere los hooks

--- repaso_examen/screens/Principal.js
+++ repaso_examen/screens/Principal.js
@@ -8,67 +8,100 @@ import {
   Image,
   FlatList,
   Pressable,
+  Alert,
 } from "react-native";
 
 export default function App({ navigation }) {
-    const data = [
-        { id: "1", title: "Real Madrid" },
-        { id: "2", title: "FC Barcelona" },
-        { id: "3", title: "Atlético de Madrid" },
-        { id: "4", title: "Sevilla FC" },
-        { id: "5", title: "Real Sociedad" },
-        { id: "6", title: "Villarreal CF" },
-        { id: "7", title: "Real Betis" },
-        { id: "8", title: "Athletic Club" },
-        { id: "9", title: "Valencia CF" },
-        { id: "10", title: "Celta de Vigo" },
-      ];
-      
+  const data = [
+    { id: "1", title: "Real Madrid" },
+    { id: "2", title: "FC Barcelona" },
+    { id: "3", title: "Atlético de Madrid" },
+    { id: "4", title: "Sevilla FC" },
+    { id: "5", title: "Real Sociedad" },
+    { id: "6", title: "Villarreal CF" },
+    { id: "7", title: "Real Betis" },
+    { id: "8", title: "Athletic Club" },
+    { id: "9", title: "Valencia CF" },
+    { id: "10", title: "Celta de Vigo" },
+  ];
+
+  const nombres = ["Juan", "Ana", "Luis"];
 
   return (
-    <View style={styles.container}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={{ alignItems: "center" }}
+    >
       <Text style={styles.titulo}>Pantalla Principal</Text>
+      <View>
+        <ScrollView
+          style={{ maxHeight: 150, maxWidth: 350 }}
+          contentContainerStyle={{ alignItems: "center" }}
+        >
+          <Text style={{ marginTop: 15 }}>Hola soy vinicius</Text>
+          <Image
+            style={{ width: 150, height: 150, resizeMode: "center" }}
+            source={require("../assets/vini.jpg")}
+          />
+          <Text style={{ marginTop: 15 }}>Hola soy vinicius</Text>
+          <Image
+            style={{ width: 150, height: 50, resizeMode: "contain" }}
+            source={require("../assets/vini.jpg")}
+          />
+          <Text style={{ marginTop: 15 }}>Hola soy vinicius</Text>
+          <Image
+            style={{ width: 150, height: 50, resizeMode: "stretch" }}
+            source={require("../assets/vini.jpg")}
+          />
+        </ScrollView>
+      </View>
 
-      <ScrollView
-        style={{ maxHeight: 150, maxWidth: 350 }}
-        contentContainerStyle={{ alignItems: "center" }}
-      >
-        <Text style={{ marginTop: 15 }}>Hola soy vinicius</Text>
-        <Image
-          style={{ width: 150, height: 150, resizeMode: "center" }}
-          source={require("../assets/vini.jpg")}
+      <View>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id} // Establece una clave única para cada elemento
+          renderItem={({ item }) => (
+            <Text style={styles.textoFlatList}>{item.title}</Text>
+          )}
+          style={{ margin: 20, maxHeight: 150 }}
         />
-        <Text style={{ marginTop: 15 }}>Hola soy vinicius</Text>
-        <Image
-          style={{ width: 150, height: 50, resizeMode: "contain" }}
-          source={require("../assets/vini.jpg")}
-        />
-        <Text style={{ marginTop: 15 }}>Hola soy vinicius</Text>
-        <Image
-          style={{ width: 150, height: 50, resizeMode: "stretch" }}
-          source={require("../assets/vini.jpg")}
-        />
-      </ScrollView>
+      </View>
 
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id} // Establece una clave única para cada elemento
-        renderItem={({ item }) => (
-          <Text style={styles.textoFlatList}>{item.title}</Text>
-        )}
-        style={{ margin: 20, maxHeight: 150 }}
-      />
+      <View style={styles.nombres}>
+        {nombres.map((nombre, index) => (
+          <Text key={index} style={styles.nombre}>
+            {nombre}
+          </Text>
+        ))}
+      </View>
 
       <Pressable
         style={styles.pressable}
-        onPress={() => console.log("Pressable pulsado")}
+        onPress={() =>
+          Alert.alert(
+            "Boton Presionado",
+            "Has presionado el boton, henorabuena!!"
+          )
+        }
       >
         <Text style={styles.textoPressable}>Presióname</Text>
       </Pressable>
 
+      <View style={styles.container2}>
+        <View style={styles.box1}>
+          <Text style={styles.text}>Box 1</Text>
+        </View>
+        <View style={styles.box2}>
+          <Text style={styles.text}>Box 2</Text>
+        </View>
+        <View style={styles.box3}>
+          <Text style={styles.text}>Box 3</Text>
+        </View>
+      </View>
+
       <Button title="Ir a Hooks" onPress={() => navigation.navigate("Hooks")} />
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -76,8 +109,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 80,
   },
 
   titulo: {
@@ -117,6 +149,65 @@ const styles = StyleSheet.create({
   textoPressable: {
     color: "#fff",
     fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  nombres: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    width: "80%",
+    maxWidth: 350,
+  },
+
+  nombre: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#0c3c5d",
+    padding: 10,
+    backgroundColor: "#f2f2f2",
+    borderRadius: 5,
+    marginBottom: 5,
+    textAlign: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    borderWidth: 1,
+    borderColor: "#0c3c5d",
+  },
+
+  container2: {
+    flex: 1, // Ocupa todo el espacio disponible
+    flexDirection: "row", // Elementos alineados en fila (horizontal)
+    justifyContent: "space-around", // Espaciado uniforme entre los elementos
+    alignItems: "center", // Centra los elementos verticalmente
+    backgroundColor: "#f0f0f0",
+  },
+  box1: {
+    width: 80,
+    height: 80,
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box2: {
+    width: 80,
+    height: 80,
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  box3: {
+    width: 80,
+    height: 80,
+    backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
     fontWeight: "bold",
   },
 });
